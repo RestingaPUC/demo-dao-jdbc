642932fb4b08b38e610c4fa27edfe0e9d70bee03
RestingaPUC <carneiroalberto@yahoo.com.br>
2022-09-03 01:16:35 -0300

    num sei 3

--- App/Program.java
+++ App/Program.java
@@ -1,11 +1,17 @@
 package App;
 
+import java.util.Date;
+
 import modelEntities.Department;
+import modelEntities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }
