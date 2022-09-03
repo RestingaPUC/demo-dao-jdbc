fe09bf77aff023c0df1b84bc4dc92ec2e2f2e12c
RestingaPUC <carneiroalberto@yahoo.com.br>
2022-09-03 00:54:45 -0300

    num sei 2

--- App/Program.java
+++ App/Program.java
@@ -1,8 +1,11 @@
 package App;
 
+import modelEntities.Department;
+
 public class Program {
     public static void main(String[] args) {
         
-        
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
