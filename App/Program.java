384117eb3abb4c2eb517372e96b5bcf0b6fdb0f9
RestingaPUC <carneiroalberto@yahoo.com.br>
2022-09-04 02:32:28 -0300

    num sei 12

--- App/Program.java
+++ App/Program.java
@@ -2,6 +2,7 @@ package App;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import modelDao.DaoFactory;
 import modelDao.SellerDao;
@@ -10,6 +11,8 @@ import modelEntities.Seller;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -37,9 +40,18 @@ public class Program {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller update ===");
-        seller = sellerDao.findById(1);
-        seller.setName("Martha Waine");
+        seller = sellerDao.findById(12);
+        seller.setName("Bosta");
         sellerDao.update(seller);
         System.out.println("Update completed");
+        System.out.println(seller);
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
